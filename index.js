4953d6a5290864dedce4cbe7fcf3b2edcda55d78
BojanSibarSymphony <35395013+BojanSibarSymphony@users.noreply.github.com>
2020-06-08 22:05:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.use("/", require("./routes/index"));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Node is running on the port  ${PORT}`));
